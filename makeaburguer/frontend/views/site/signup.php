9a1eb30af33b10128332927f15ad01656d921f08
Rodolfo Barreira <rodolfo-barreira@hotmail.com>
2019-10-29 13:02:38 +0000

    update no signup
    a

--- makeaburguer/frontend/views/site/signup.php
+++ makeaburguer/frontend/views/site/signup.php
@@ -23,6 +23,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'email') ?>
 
+                <?= $form->field($model,'nif') ?>
+
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
 
