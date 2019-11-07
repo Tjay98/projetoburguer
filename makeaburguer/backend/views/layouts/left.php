faafcbbfed9d438a1e4ffae9124d032a927f35e3
Rodolfo Barreira <rodolfo-barreira@hotmail.com>
2019-11-07 16:46:31 +0000

    Pagina de signup
    Solução temporária ACF para funcionamento do registo e login de utilizadores (codigo antigo comentado)

--- makeaburguer/backend/views/layouts/left.php
+++ makeaburguer/backend/views/layouts/left.php
@@ -5,7 +5,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="<?= $directoryAsset ?>/img/avatar-test2.png" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
                 <p>NOME TEMPORARIO</p>
@@ -30,7 +30,7 @@
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+                    ['label' => 'Menu', 'options' => ['class' => 'header']],
                     ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
                     ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
