482fff652a55c110e0c8a55d8b40feed1b62092d
Rodolfo Barreira <rodolfo-barreira@hotmail.com>
2019-11-08 17:26:16 +0000

    remover theme admin lte
    remoçao do tema predefinido admin lte

--- makeaburguer/backend/config/main.php
+++ makeaburguer/backend/config/main.php
@@ -14,13 +14,6 @@ return [
   //  'homeUrl' => '/makeaburguer/admin',
     'modules' => [],
     'components' => [
-        'view' => [
-            'theme' => [
-                'pathMap' => [
-                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
-                ],
-            ],
-        ],
         'request' => [
             'csrfParam' => '_csrf-backend',
             //'baseUrl'=>'makeaburguer/admin'
