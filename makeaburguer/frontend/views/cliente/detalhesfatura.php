e49a0d869ff4fea19c2213b1c21723a1c94d691c
Rodolfo Barreira <rodolfo-barreira@hotmail.com>
2020-01-13 17:42:48 +0000

    fim dos detalhes da fatura

--- makeaburguer/frontend/views/cliente/detalhesfatura.php
+++ makeaburguer/frontend/views/cliente/detalhesfatura.php
@@ -19,29 +19,160 @@ $this->title = 'pedido';
         <?php if(!empty($pedidos)){ ?>
 
             <?php
-                    echo $pedidos->data;
-                    echo "<br>";
-                    echo $pedidos->id_menu;
+                    echo '<h5 style="color:white">'.$pedidos->data.'</h5>';
+                    echo '<p style="color:white">Id do menu: '.$pedidos->id_menu.'</p>';
                 
+                    echo'<table class="table table-striped table-bordered detail-view">
+                                <tbody>
+                                    <tr>
+                                        <th>Produto</th>
+                                        <th>Valor</th>
+                                        <th>Detalhes</th>
+                                    </tr>';
+                    if(!empty($hamburguer)){
+                        echo '<tr>';
+                        echo '<td>'.$hamburguer->nome.'</td>';
+                        echo '<td>'.$hamburguer->preco.'</td>';
+                        echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#hamburguer">Detalhes</button></td>';
+                        echo '</tr>';
+                        ?>
+                        <div class="modal fade" id="hamburguer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h3 class="modal-title"><center><?=$hamburguer->nome?></center></h3>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                        <div class="modal-body">
+                                        <center><?php echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$hamburguer->imagem,['class'=>'imagemproduto']);?></center>
+                                        <br>
+                                        <center><h4>Descrição:</h4></center>
+                                        <center> <?= $hamburguer->descricao?> </center>
+                                        <br>
+                                        <center><h5>Preço: <?= $hamburguer->preco?>€</h5></center>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
                         
+                    <?php
+                    }                
                     if(!empty($bebida)){
-                        echo $bebida->nome;
-                        echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$bebida->imagem,['class'=>'imagemproduto']);
+                        echo '<tr>';
+                        echo '<td>'.$bebida->nome.'</td>';
+                        echo '<td>'.$bebida->preco.'</td>';
+                        echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#bebida">Detalhes</button></td>';
+                        echo '</tr>';
+                        ?>
+                        <div class="modal fade" id="bebida" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title"><center><?=$bebida->nome?></center></h1>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                        <div class="modal-body">
+                                        <center><?php echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$bebida->imagem,['class'=>'imagemproduto']);?></center>
+                                        <br>
+                                        <center><h5>Preço: <?= $bebida->preco?>€</h5></center>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    <?php
                     }
                     if(!empty($complemento)){
-                        echo $complemento->nome;
-                        echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$complemento->imagem,['class'=>'imagemproduto']);
+                        echo '<tr>';
+                        echo '<td>'.$complemento->nome.'</td>';
+                        echo '<td>'.$complemento->preco.'</td>';
+                        echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#complemento">Detalhes</button></td>';
+                        echo '</tr>';
+                        ?>
+                        <div class="modal fade" id="complemento" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title"><center><?=$complemento->nome?></center></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                        <div class="modal-body">
+                                        <center><?php echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$complemento->imagem,['class'=>'imagemproduto']);?></center>
+                                        <br>
+                                        <center><h5>Preço: <?= $bebida->preco?>€</h5></center>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    <?php
+                    
                     }
                     if(!empty($sobremesa)){
-                        echo $sobremesa->nome;
-                        echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$sobremesa->imagem,['class'=>'imagemproduto']);
-                    }
+                        echo '<tr>';
+                        echo '<td>'.$sobremesa->nome.'</td>';
+                        echo '<td>'.$sobremesa->preco.'</td>';
+                        echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#sobremesa">Detalhes</button></td>';
+                        echo '</tr>';
+                        ?>
+                        <div class="modal fade" id="sobremesa" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title"><center><?=$sobremesa->nome?></center></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                        <div class="modal-body">
+                                        <center><?php echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$sobremesa->imagem,['class'=>'imagemproduto']);?></center>
+                                        <br>
+                                        <center><h5>Preço: <?= $sobremesa->preco?>€</h5></center>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    <?php }
                     if(!empty($extra)){
-                        echo $extra->nome;
-                        echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$extra->imagem,['class'=>'imagemproduto']);
-                    } 
+                        echo '<tr>';
+                        echo '<td>'.$extra->nome.'</td>';
+                        echo '<td>'.$extra->preco.'</td>';
+                        echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#extra">Detalhes</button></td>';
+                        echo '</tr>';
+                        ?>
+                        <div class="modal fade" id="extra" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title"><center><?=$extra->nome?></center></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                        <div class="modal-body">
+                                        <center><?php echo Html::img(Yii::$app->request->baseUrl.'/backend/web/'.$extra->imagem,['class'=>'imagemproduto']);?></center>
+                                        <br>
+                                        <center><h5>Preço: <?= $extra->preco?>€</h5></center>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } 
+                    echo '</tbody></table>';
                     ?>
-        <?php }else{
+                    <!-- Button trigger modal -->
+
+
+
+        <?php }
+        else{
             echo '<center><h2>Não selecionou nenhum pedido</h2></center>';
         } ?>  
       
