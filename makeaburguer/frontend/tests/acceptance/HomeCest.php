1aef68eaee1089b738359435aed13b6bf7cbf6c2
Rodolfo Barreira <rodolfo-barreira@hotmail.com>
2020-01-17 02:13:28 +0000

    acceptance curtinho

--- makeaburguer/frontend/tests/acceptance/HomeCest.php
+++ makeaburguer/frontend/tests/acceptance/HomeCest.php
@@ -9,19 +9,23 @@ class HomeCest
     public function checkHome(AcceptanceTester $I)
     {
         $I->amOnPage('/');
+        $I->wait(5);
         //$I->see('veja os nossos produtos');
         $I->seeLink('Login');
         $I->click('Login');
 
       //  $I->seeLink('About');
        // $I->click('About');
-        $I->wait(10); // wait for page to be opened
+        $I->wait(5); // wait for page to be opened
         $I->fillField('#loginform-username', 'rodolfo');
         $I->fillField('#loginform-password', '123456789');
-        $I->click('Login');
-        $I->wait(10);
+        $I->click('login-button');
+        $I->wait(5);
         $I->see('perfil');
-        $I->wait(10);
+        $I->wait(5);
+        $I->seeLink('Logout');
+        $I->click('Logout');
+        $I->wait(5);
         //$I->see('This is the About page.');
     }
 }
